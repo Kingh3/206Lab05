c9a3b9b42287221c2ac3671324e2328eede52f9b
hassanAlhamaqi <dev.hassan.alhamaqi@gmail.com>
2022-10-09 20:25:03 +0300

    changes

--- Sorter.java
+++ Sorter.java
@@ -1,19 +1,39 @@
 import java.util.Arrays;
-
 public class Sorter {
-	public static void main(String[] args) {
-		
-		System.out.println("hello");
+   public static void main(String[] args) {
+      System.out.println("hello");
+      int[] arr1 = {9,14,3,2,43,11,58,22};
         int[] arr = {2,1,5,4};
         selectionSort(arr);
+        insertionSort(arr1);
         System.out.println(Arrays.toString(arr));
-	
-	}
-	
-    static void selectionSort(int arr[])
-    {
+        System.out.println(Arrays.toString(arr1));
+    }
+
+
+    private static void insertionSort(int[] array) {
+
+            int n = array.length;
+            for (int j = 1; j < n; j++) {
+                int key = array[j];
+                int i = j-1;
+                while ( (i > -1) && ( array [i] > key ) ) {
+                    array [i+1] = array [i];
+                    i--;
+                }
+                array[i+1] = key;
+            }
+        }
+
+
+
+   
+
+    static void selectionSort(int[] arr){
+
+   
+
         int n = arr.length;
- 
         // One by one move boundary of unsorted subarray
         for (int i = 0; i < n-1; i++)
         {
@@ -22,7 +42,6 @@ public class Sorter {
             for (int j = i+1; j < n; j++)
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
- 
             // Swap the found minimum element with the first
             // element
             int temp = arr[min_idx];
