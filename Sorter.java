df6bf5999719744b0b336c55ee3eef3a4582b7c9
hassanAlhamaqi <dev.hassan.alhamaqi@gmail.com>
2022-10-09 19:32:14 +0300

    second commit

--- Sorter.java
+++ Sorter.java
@@ -1,9 +1,32 @@
+import java.util.Arrays;
+
 public class Sorter {
 	public static void main(String[] args) {
-		System.out.println("hello");
 		
+		System.out.println("hello");
+        int[] arr = {2,1,5,4};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
 	
 	}
-	
-
+    static void sort(int arr[])
+    {
+        int n = arr.length;
+ 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
